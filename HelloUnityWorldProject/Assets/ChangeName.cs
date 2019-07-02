dc8ebd88c1dda1f5848894bc4221c188238a77d4
Annalise Alvarez <afalvarez0821@gmail.com>
2019-07-01 20:37:50 -0400

    updated project

---
+++ HelloUnityWorldProject/Assets/ChangeName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ChangeName : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            TextMeshProUGUI textmeshPro = GetComponent<TextMeshProUGUI>();
+            textmeshPro.SetText("Hello World from Anna Alvarez");
+        }
+        
+    }
+}
